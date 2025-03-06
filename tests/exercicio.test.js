a51d4465825b6101c0fcee34160ba769f06c95f4
Emerson <70976306+emerdesenv@users.noreply.github.com>
2025-03-06 10:23:56 -0300

    refactor: Ajustes docs

--- tests/exercicio.test.js
+++ tests/exercicio.test.js
@@ -3,9 +3,7 @@ const { extrairTitulos } = require('../src/map');
 const request = require('supertest');
 
 const API_URL = 'https://jsonplaceholder.typicode.com/';
-
 describe('Testes de Integração e Unitários - API JSONPlaceholder', () => {
-
     // Teste Unitário usando os dados da API
     test('Deve extrair os títulos dos posts retornados da API', async () => {
         const response = await request(API_URL).get('posts'); // Busca os posts da API
@@ -20,7 +18,6 @@ describe('Testes de Integração e Unitários - API JSONPlaceholder', () => {
         expect(titulos.length).toBe(posts.length); // O número de títulos deve ser igual ao número de posts
         expect(titulos[0]).toBe(posts[0].title); // O primeiro título deve ser o mesmo do primeiro post
     });
-
     test('Deve acusar erro na função.', async () => {
         expect(() => extrairTitulos(false)).toThrow("A entrada precisa ser um array de objetos");
     });
